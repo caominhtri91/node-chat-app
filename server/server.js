1e035ec0a5e793b74c4ea0e5dd53eb55f0e9b052
Tri <caominhtri91@gmail.com>
2017-03-22 22:05:42 +0700

    Add join page that submits to chat.html

--- server/server.js
+++ server/server.js
@@ -3,38 +3,38 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage, generateLocationMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-	console.log('new user connected');
+  console.log('new user connected');
 
-	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-	socket.on('createMessage', (message, callback) => {
-		console.log('createMessage', message);
-		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback();
-	});
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
 
-	socket.on('createLocationMessage', (coords) => {
-		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-	});
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
-	socket.on('disconnect', () => {
-		console.log('User disconnected from server');
-	});
+  socket.on('disconnect', () => {
+    console.log('User disconnected from server');
+  });
 });
 
 server.listen(port, () => {
-	console.log(`Started server on port ${port}`);
+  console.log(`Started server on port ${port}`);
 });
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,19 +1,18 @@
 const moment = require('moment');
 
 var generateMessage = (from, text) => {
-	return {
-		from,
-		text,
-		createdAt: moment().valueOf()
-	};
+    return {from, text, createdAt: moment().valueOf()};
 };
 
 var generateLocationMessage = (from, latitude, longitude) => {
-	return {
-		from,
-		url: `http://google.com/maps?q=${latitude},${longitude}`,
-		createdAt: moment().valueOf()
-	};
+  return {
+    from,
+    url: `http://google.com/maps?q=${latitude},${longitude}`,
+    createdAt: moment().valueOf()
+  };
 };
 
-module.exports = {generateMessage, generateLocationMessage};
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+};
